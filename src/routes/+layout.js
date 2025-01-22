d3a76dc92e8961050c2d91eafc9d9bfe6fefa487
Neha <nehadesaraju@gmail.com>
2025-01-21 22:10:45 -0800

    add analytics

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,1 +1,15 @@
+import posthog from 'posthog-js'
+import { browser } from '$app/environment';
+
+export const load = async () => {
+
+  if (browser) {
+    posthog.init(
+      'phc_BM3j0JF6a55KimKJ439Qc7iZaDtDlmq1UVYxDk1J7TG',
+      { api_host: 'https://us.i.posthog.com', person_profiles: 'identified_only' }
+    )
+  }
+  return
+};
+
 export const prerender = true;
